55a84048a0ab724ab2312a4070a0f1dc1e49e750
MUHAMMAD HAMMAD TAHIR <113124893+hammadtahir0000@users.noreply.github.com>
2024-06-27 14:07:47 +0500

    acs

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,61 +1,7 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import image404 from "../assets/img/404.jpg";
 
-// const NotFound = () => {
-//   return (
-//     <div className="error_page">
-//       <div
-//         className="hero bg-image"
-//         style={{
-//           backgroundImage: `url(${process.env.PUBLIC_URL + image404})`,
-//         }}
-//       >
-//         <div className="content">
-//           <h1 data-aos="fade-up" data-aos-duration="1200">
-//             404!
-//           </h1>
-//           <p data-aos="fade-up" data-aos-duration="1200" data-aos-delay="50">
-//             The page you are looking for could not be found.
-//           </p>
-
-//           <div
-//             className="button"
-//             data-aos="fade-up"
-//             data-aos-duration="1200"
-//             data-aos-delay="100"
-//           >
-//             <Link to="/">BACK TO HOME</Link>
-//           </div>
-//           {/* End purchase_button */}
-//         </div>
-//       </div>
-//       {/* End .hero */}
-//     </div>
-//   );
-// };
-
-// export default NotFound;
-// import React, { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// const NotFound = () => {
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     // Immediately redirect to home page
-//     navigate("/HomeDark");
-//   }, [navigate]);
-
-//   // No JSX content returned when performing immediate redirect
-
-//   return null;
-// };
-
-// export default NotFound;
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import HomeDark from "../views/all-home-version/HomeDark";
 const NotFound = () => {
   const navigate = useNavigate();
 
