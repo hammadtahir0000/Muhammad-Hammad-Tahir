3895267889caecfc749e6dd86aecf86be04eec61
MUHAMMAD HAMMAD TAHIR <113124893+hammadtahir0000@users.noreply.github.com>
2024-06-27 11:23:49 +0500

    fixeseses

--- src/router/AllRoutes.js
+++ src/router/AllRoutes.js
@@ -1,23 +1,61 @@
-import React from "react";
-import Preview from "../views/Preview";
+// import React from "react";
+// import Preview from "../views/Preview";
+// import HomeDark from "../views/all-home-version/HomeDark";
+// import HomeRtl from "../views/all-home-version/HomeRtl";
+// import NotFound from "../views/NotFound";
+// import { Routes, Route } from "react-router-dom";
+// import ContextProvider from "../Context/ContextProvider";
+// import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
+
+// const AllRoutes = () => {
+//   return (
+//     <>
+//       <ContextProvider>
+//         <ScrollTopBehaviour />
+//         <Routes>
+//           <Route path="/" element={<HomeDark />} />
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </ContextProvider>
+//     </>
+//   );
+// };
+
+// export default AllRoutes;
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import HomeDark from "../views/all-home-version/HomeDark";
-import HomeRtl from "../views/all-home-version/HomeRtl";
 import NotFound from "../views/NotFound";
-import { Routes, Route } from "react-router-dom";
 import ContextProvider from "../Context/ContextProvider";
 import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
 
+const RedirectToHome = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  
+  useEffect(() => {
+    const specificURL = "https://hammadtahir0000.github.io/Muhammad-Hammad-Tahir/";
+    
+    if (location.pathname === specificURLPath) {
+      navigate("/", { replace: true });
+    }
+  }, [location, navigate]);
+
+  return null;
+};
+
 const AllRoutes = () => {
   return (
-    <>
+    <Router>
       <ContextProvider>
         <ScrollTopBehaviour />
+        <RedirectToHome />
         <Routes>
           <Route path="/" element={<HomeDark />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </ContextProvider>
-    </>
+    </Router>
   );
 };
 
