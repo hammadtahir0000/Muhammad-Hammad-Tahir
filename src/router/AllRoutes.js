8bd6436b22d8da5a40ad416baa446ad9a8258603
MUHAMMAD HAMMAD TAHIR <113124893+hammadtahir0000@users.noreply.github.com>
2024-06-27 11:48:57 +0500

    routssss

--- src/router/AllRoutes.js
+++ src/router/AllRoutes.js
@@ -22,8 +22,46 @@
 // };
 
 // export default AllRoutes;
+// import React, { useEffect } from "react";
+// import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+// import HomeDark from "../views/all-home-version/HomeDark";
+// import NotFound from "../views/NotFound";
+// import ContextProvider from "../Context/ContextProvider";
+// import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
+
+// const RedirectToHome = () => {
+//   const navigate = useNavigate();
+//   const location = useLocation();
+  
+//   useEffect(() => {
+//     const specificURL = "https://hammadtahir0000.github.io/Muhammad-Hammad-Tahir/";
+    
+//     if (location.pathname === specificURL) {
+//       navigate("/", { replace: true });
+//     }
+//   }, [location, navigate]);
+
+//   return null;
+// };
+
+// const AllRoutes = () => {
+//   return (
+//     <Router>
+//       <ContextProvider>
+//         <ScrollTopBehaviour />
+//         <RedirectToHome />
+//         <Routes>
+//           <Route path="/" element={<HomeDark />} />
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </ContextProvider>
+//     </Router>
+//   );
+// };
+
+// export default AllRoutes;
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import HomeDark from "../views/all-home-version/HomeDark";
 import NotFound from "../views/NotFound";
 import ContextProvider from "../Context/ContextProvider";
@@ -34,9 +72,9 @@ const RedirectToHome = () => {
   const location = useLocation();
   
   useEffect(() => {
-    const specificURL = "https://hammadtahir0000.github.io/Muhammad-Hammad-Tahir/";
+    const specificURLPath = "/Muhammad-Hammad-Tahir/";
     
-    if (location.pathname === specificURL) {
+    if (location.pathname === specificURLPath) {
       navigate("/", { replace: true });
     }
   }, [location, navigate]);
