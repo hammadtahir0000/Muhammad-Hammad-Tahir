2f25eef6b0f3ac1544958a05c9c55b0ac6cf587b
MUHAMMAD HAMMAD TAHIR <113124893+hammadtahir0000@users.noreply.github.com>
2024-06-27 12:02:13 +0500

    final2

--- src/router/AllRoutes.js
+++ src/router/AllRoutes.js
@@ -1,27 +1,28 @@
-// import React from "react";
-// import Preview from "../views/Preview";
-// import HomeDark from "../views/all-home-version/HomeDark";
-// import HomeRtl from "../views/all-home-version/HomeRtl";
-// import NotFound from "../views/NotFound";
-// import { Routes, Route } from "react-router-dom";
-// import ContextProvider from "../Context/ContextProvider";
-// import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
+import React from "react";
+import Preview from "../views/Preview";
+import HomeDark from "../views/all-home-version/HomeDark";
+import HomeRtl from "../views/all-home-version/HomeRtl";
+import NotFound from "../views/NotFound";
+import { Routes, Route } from "react-router-dom";
+import ContextProvider from "../Context/ContextProvider";
+import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
 
-// const AllRoutes = () => {
-//   return (
-//     <>
-//       <ContextProvider>
-//         <ScrollTopBehaviour />
-//         <Routes>
-//           <Route path="/" element={<HomeDark />} />
-//           <Route path="*" element={<NotFound />} />
-//         </Routes>
-//       </ContextProvider>
-//     </>
-//   );
-// };
+const AllRoutes = () => {
+  return (
+    <>
+      <ContextProvider>
+        <ScrollTopBehaviour />
+        <Routes>
+          <Route path="/" element={<HomeDark />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ContextProvider>
+    </>
+  );
+};
 
-// export default AllRoutes;
+export default AllRoutes;
+
 // import React, { useEffect } from "react";
 // import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 // import HomeDark from "../views/all-home-version/HomeDark";
@@ -60,41 +61,41 @@
 // };
 
 // export default AllRoutes;
-import React, { useEffect } from "react";
-import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
-import HomeDark from "../views/all-home-version/HomeDark";
-import NotFound from "../views/NotFound";
-import ContextProvider from "../Context/ContextProvider";
-import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
+// import React, { useEffect } from "react";
+// import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+// import HomeDark from "../views/all-home-version/HomeDark";
+// import NotFound from "../views/NotFound";
+// import ContextProvider from "../Context/ContextProvider";
+// import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
 
-const RedirectToHome = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
+// const RedirectToHome = () => {
+//   const navigate = useNavigate();
+//   const location = useLocation();
   
-  useEffect(() => {
-    const specificURLPath = "https://hammadtahir0000.github.io/Muhammad-Hammad-Tahir/";
+//   useEffect(() => {
+//     const specificURLPath = "https://hammadtahir0000.github.io/Muhammad-Hammad-Tahir/";
     
-    if (location.pathname === specificURLPath) {
-      navigate("/", { replace: true });
-    }
-  }, [location, navigate]);
+//     if (location.pathname === specificURLPath) {
+//       navigate("/", { replace: true });
+//     }
+//   }, [location, navigate]);
 
-  return null;
-};
+//   return null;
+// };
 
-const AllRoutes = () => {
-  return (
-    <Router>
-      <ContextProvider>
-        <ScrollTopBehaviour />
-        <RedirectToHome />
-        <Routes>
-          <Route path="/" element={<HomeDark />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </ContextProvider>
-    </Router>
-  );
-};
+// const AllRoutes = () => {
+//   return (
+//     <Router>
+//       <ContextProvider>
+//         <ScrollTopBehaviour />
+//         <RedirectToHome />
+//         <Routes>
+//           <Route path="/" element={<HomeDark />} />
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </ContextProvider>
+//     </Router>
+//   );
+// };
 
-export default AllRoutes;
+// export default AllRoutes;
